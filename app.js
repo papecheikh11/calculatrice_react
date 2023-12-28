0bdb6fbc6c6d793e05d9d07da48f40d94c44efb7
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2023-12-28 09:00:28 +0000

    first commit

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="container">
+      <div className="container w-25">
         <form>
           <input
             type="text"
